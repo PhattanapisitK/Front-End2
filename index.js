37e4f929650c44f6edc0b41b93e86a6c59ef6279
PhattanapisitK <s6506021620091@email.kmutnb.ac.th>
2024-02-13 10:29:17 +0700

    .

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+// index.js
 // Description: Node.js HTML client
 // requires: npm install express ejs axios body-parser
 
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+var bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-var bodyParser = require('body-parser');
 
 // Base URL for the API
 // const base_url = "https://api.example.com";
 const base_url = "http://node57538-phattanapisitk.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 
 app.get("/", async (req, res) => {
